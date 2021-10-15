b3c7b0c47a74fb7f53db9fbb43b0bf5b1d57531c
Prashant Kumar <pk2psp@gmail.com>
2021-10-15 11:42:07 +0530

    Just a playaround

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+class Board extends React.Component {
+    // component registeration
+    renderSquare(i) {
+        return <Square valueName={i} />;
+    }
+    render() {
+        const status = 'Next player: X';
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {/* // Direct using component */}
+                    <Square valueName={3} />
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            valueName: null,
+        }
+    }
+    recordClick(no) {
+        return () => { this.setState({ valueName: `${no}X` }) };
+    }
+    render() {
+        // inside onClick you have to define () => { }, so you can use external function like line no. 37
+        return <button className="square" onClick={this.recordClick(this.props.valueName)}>{this.state.valueName}</button>;
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
